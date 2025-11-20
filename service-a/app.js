3ffec9e230e47e98e59324704dc52172612412be
akashsinghcs23-create <akash.singh_cs23@gla.ac.in>
2025-11-20 20:55:34 +0530

    Force add requirements.txt for service-b and update service-a

--- service-a/app.js
+++ service-a/app.js
@@ -7,4 +7,4 @@ const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => res.send("hello from service-a"));
 
-app.listen(port, () => console.log(`service-a http://localhost:${port}`));
+app.listen(port, "0.0.0.0", () => console.log(`service-a http://localhost:${port}`));
