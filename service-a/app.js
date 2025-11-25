d3adf8474d3ce80e1188d4e013c43dd0c91f2d5a
Akash86500# <akchaudhary86500@gmail.com>
2025-11-25 12:44:13 +0530

    Added: New UI

--- service-a/app.js
+++ service-a/app.js
@@ -1,12 +1,11 @@
 import express from "express";
 import cors from "cors";
-import morgan from "morgan";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(morgan("dev"));
+
 
 app.get("/", (req, res) => {
   const htmlContent = `
